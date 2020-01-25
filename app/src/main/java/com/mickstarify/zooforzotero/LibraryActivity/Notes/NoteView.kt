6e99f7db39e761bc1251f176309f4906386c0a0d
Michael Johnston <michael.johnston29@gmail.com>
2020-01-25 12:54:07 +1100

    made notes text selectable

--- app/src/main/java/com/mickstarify/zooforzotero/LibraryActivity/Notes/NoteView.kt
+++ app/src/main/java/com/mickstarify/zooforzotero/LibraryActivity/Notes/NoteView.kt
@@ -3,6 +3,8 @@ package com.mickstarify.zooforzotero.LibraryActivity.Notes
 import android.app.AlertDialog
 import android.content.Context
 import android.text.Html
+import android.util.Log
+import android.widget.TextView
 import com.mickstarify.zooforzotero.ZoteroAPI.Model.Note
 
 class NoteView(val context: Context, val note: Note, val listener: NoteInteractionListener) {
@@ -10,12 +12,18 @@ class NoteView(val context: Context, val note: Note, val listener: NoteInteracti
     }
 
     fun show() {
-        AlertDialog.Builder(context)
+        val dialog = AlertDialog.Builder(context)
             .setTitle("Note")
             .setMessage(Html.fromHtml(note.note))
             .setPositiveButton("Dismiss", { _, _ -> })
             .setNeutralButton("Edit", { _, _ -> showNoteEditingDialog() })
             .show()
+        try{
+            val textView = dialog.window?.decorView?.findViewById<TextView>(android.R.id.message)
+            textView?.setTextIsSelectable(true)
+        } catch (e: Exception){
+            Log.e("zotero", "error setting text selectable in note, $e")
+        }
     }
 
     fun showNoteEditingDialog() {
